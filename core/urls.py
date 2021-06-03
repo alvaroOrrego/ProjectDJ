42cb632e0cfee61294793145b6eb3ff9bfcaeb50
aorre6o <alv.orrego@duocuc.cl>
2021-06-03 17:29:12 -0400

    Url y vistas (principal, contacto) + linkeo en html PagPrincipal hacia Contacto

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import principal, contacto
+
+
+urlpatterns = [
+    path('', principal, name="principal"),
+    path('contacto/', contacto, name="contacto"),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def principal(request):
+    return render (request, 'core/PaginaPrincipal.html')
+
+def contacto(request):
+    return render(request, 'core/Contacto.html')
